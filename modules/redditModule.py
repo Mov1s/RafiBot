cec7a6294aa1fd6b4e67e2106ce68d3cd13b4e86
Ryan Popa <ryanp@recsolu.com>
2013-01-03 13:13:55 -0600

    Fixing the annoying redditModule crash, related to connection expiring or something, doesn't seem to throw real exception

--- modules/redditModule.py
+++ modules/redditModule.py
@@ -9,20 +9,24 @@ def main(irc):
 	message = irc.lastMessage()
 
 	#If no room activity for 10 mins link the top rated carPorn picture if it hasn't already been linked
-	#600
-	if noRoomActivityForTime(irc, 10):
-		urlFormat = '/r/carporn'
-		redditConnection.request('GET', urlFormat)
-		responseBody = redditConnection.getresponse()
-		responseBodyString = responseBody.read()
+	if noRoomActivityForTime(irc, 600):
+		try:
+			urlFormat = '/r/carporn'
+			redditConnection.request('GET', urlFormat)
+			responseBody = redditConnection.getresponse()
+			responseBodyString = responseBody.read()
 
-		#Find all of the links
-		soup = BeautifulSoup(responseBodyString)
-		anchors = soup.find_all('a', "title")
+			#Find all of the links
+			soup = BeautifulSoup(responseBodyString)
+			anchors = soup.find_all('a', "title")
 
-		#Show the top link if it hasn't already been posted
-		global lastPostedLink
-		topRedditLink = anchors[0]['href']
-		if topRedditLink != lastPostedLink:
-			lastPostedLink = topRedditLink
-			irc.sendMessage(topRedditLink)
+			#Show the top link if it hasn't already been posted
+			global lastPostedLink
+			topRedditLink = anchors[0]['href']
+			if topRedditLink != lastPostedLink:
+				lastPostedLink = topRedditLink
+				irc.sendMessage(topRedditLink)
+		except:
+			#Recreate the reddit connection, this should only happen in a timeout
+			global redditConnection
+			redditConnection = httplib.HTTPConnection('www.reddit.com')
