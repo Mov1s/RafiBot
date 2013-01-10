5e558a259da410f7f2f37fe5888b59fa732d7f89
Ryan Popa <ryanp@recsolu.com>
2013-01-10 10:04:12 -0600

    Switching redditModule to use urllib2 Updating redditModule for new framework Adding all modules back into Rafi main

--- rafi.py
+++ rafi.py
@@ -1,9 +1,9 @@
 from ircBase import *
 import modules.baseModule as baseModule
-# import modules.redditModule as redditModule
-# import modules.twssModule as twssModule
-# import modules.weatherModule as weatherModule
-# import modules.imageModule as imageModule
+import modules.redditModule as redditModule
+import modules.twssModule as twssModule
+import modules.weatherModule as weatherModule
+import modules.imageModule as imageModule
 
 irc = ircConnection().newConnection()
 
@@ -14,9 +14,9 @@ while True:
    
    #Command Modules
    baseModule.main(irc)
-   # redditModule.main(irc)
-   # twssModule.main(irc)
-   # weatherModule.main(irc)
-   # imageModule.main(irc)
+   redditModule.main(irc)
+   twssModule.main(irc)
+   weatherModule.main(irc)
+   imageModule.main(irc)
    
    print irc.lastMessage().rawMessage
