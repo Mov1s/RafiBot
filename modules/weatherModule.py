bca24e76de919c2e38776ef23be4d36d3cb206fd
sdmadsen <steve@sdmadsen.com>
2013-01-14 12:29:38 -0500

    Expanded weatherModule.py
    Added !rafiBot weather to give current conditions as well as temperature.

--- modules/weatherModule.py
+++ modules/weatherModule.py
@@ -6,8 +6,8 @@ from bs4 import BeautifulSoup
 
 def main(irc):
   	message = irc.lastMessage()
-	
-	
+
+
 	#Temperature command
 	if messageIsBotCommand(message, 'temperature'):
 		try:
@@ -18,19 +18,49 @@ def main(irc):
 			responseBodyString = urllib2.urlopen(url).read()
 
 			soup = BeautifulSoup(responseBodyString)
-			
+
 			#Find Current Temperature Span
 			temperatureSpan = soup.find("span", {"id": "rapidtemp"})
 			temperatureSpan = temperatureSpan.find("span", {"class": "b"})
-			
+
 			#Find Feels Like Temperature
 			feelsLike = soup.find("div", {"id": "tempFeel"})
 			feelsLike = feelsLike.find("span", {"class": "b"})
-				
+
 			#Find Location
 			locationName = soup.find("h1", {"id": "locationName"})
 			#locationName = locationName.find("span", {"class": "b"})
-			
+
 			irc.sendMessage("It is " + temperatureSpan.string + "F, Feels Like " + feelsLike.string + "F in " + locationName.string)
+		except:
+			return
+
+	#Weather command
+	elif messageIsBotCommand(message, 'weather'):
+		try:
+			#Request Weather Underground for weather 
+			temperature = message[message.rfind('weather') + 8:].replace(' ', '+')
+			temperature = temperature.replace(',', '%2C')
+			url = "http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=" + temperature
+			responseBodyString = urllib2.urlopen(url).read()
+
+			soup = BeautifulSoup(responseBodyString)
+
+			#Find Current Temperature Span
+			temperatureSpan = soup.find("span", {"id": "rapidtemp"})
+			temperatureSpan = temperatureSpan.find("span", {"class": "b"})
+
+			#Find Feels Like Temperature
+			feelsLike = soup.find("div", {"id": "tempFeel"})
+			feelsLike = feelsLike.find("span", {"class": "b"})
+
+			#Find Current Condition
+			currentCondition = soup.find("div", {"id": "curCond"})
+
+			#Find Location
+			locationName = soup.find("h1", {"id": "locationName"})
+			#locationName = locationName.find("span", {"class": "b"})
+
+			irc.sendMessage("It is " + temperatureSpan.string + "F and " + currentCondition.string + ", Feels Like " + feelsLike.string + "F in " + locationName.string)
 		except:
 			return
