4d2878359a74c1391eb5a022fce0ce369ae92236
Steve Madsen <steve@sdmadsen.com>
2013-01-17 18:47:16 -0500

    update weatherModule keywords

--- modules/weatherModule.py
+++ modules/weatherModule.py
@@ -6,36 +6,12 @@ from bs4 import BeautifulSoup
 
 def main(irc):
   	message = irc.lastMessage()
+	
+	expression = re.compile('(temperature|temp)(me|ma)? (.*)', re.IGNORECASE)
+	match = expression.match(message)
 
 	#Temperature command
-	if message.botCommand == 'temperature':
-		try:
-			#Request Weather Underground for temperature
-			temperature = message.body[message.body.rfind('temperature') + 12:].replace(' ', '+')
-			temperature = temperature.replace(',', '%2C')
-			url = "http://www.wunderground.com/cgi-bin/findweather/hdfForecast?query=" + temperature
-			responseBodyString = urllib2.urlopen(url).read()
-
-			soup = BeautifulSoup(responseBodyString)
-
-			#Find Current Temperature Span
-			temperatureSpan = soup.find("span", {"id": "rapidtemp"})
-			temperatureSpan = temperatureSpan.find("span", {"class": "b"})
-
-			#Find Feels Like Temperature
-			feelsLike = soup.find("div", {"id": "tempFeel"})
-			feelsLike = feelsLike.find("span", {"class": "b"})
-
-			#Find Location
-			locationName = soup.find("h1", {"id": "locationName"})
-			#locationName = locationName.find("span", {"class": "b"})
-			
-			ircMessage().newRoomMessage(irc, "It is " + temperatureSpan.string + "F, Feels Like " + feelsLike.string + "F in " + locationName.string).send()
-		except:
-			return
-
-	#Weather command
-	elif message.botCommand == 'weather':
+	if match:
 		try:
 			#Request Weather Underground for weather 
 			temperature = message.body[message.body.rfind('weather') + 8:].replace(' ', '+')
