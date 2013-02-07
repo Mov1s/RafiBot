41dcb2b5ef0e420cfefeda3609db4d4f6ef843ff
Steve Madsen <steve@sdmadsen.com>
2013-02-06 20:29:23 -0500

    Added ability to respond to rafiBot texts and have them displayed in irc.

--- modules/smsModule.py
+++ modules/smsModule.py
@@ -4,10 +4,51 @@ import smtplib
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEBase import MIMEBase
 from email.MIMEText import MIMEText
+import imaplib
+import email
 
 
 CONST_CONTACT = 2
 CONST_MESSAGE = 3
+
+
+def extract_body(payload):
+	if isinstance(payload,str):
+		return payload
+	else:
+		return '\n'.join([extract_body(part.get_payload()) for part in payload])
+
+
+
+def readEmail (irc, contactList):
+	conn = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+	conn.login(contactList[0],contactList[1])
+	conn.select()
+	typ, data = conn.search(None, 'UNSEEN')
+	try:
+		for num in data[0].split():
+			typ, msg_data = conn.fetch(num, '(RFC822)')
+			for response_part in msg_data:
+				if isinstance(response_part, tuple):
+					msg = email.message_from_string(response_part[1])
+					fromAddr = msg['from']
+					payload=msg.get_payload()
+					message=extract_body(payload)
+					if len(payload) == 2:
+						print("Hello, world!")
+						attachment = payload[1]
+						message = attachment.get_payload(decode=True)
+					contact = contactList[getContactIndex(contactList, fromAddr[:10]) -1].split("|")
+					if getContactIndex(contactList, fromAddr[:10]) <> -1:	
+						ircMessage.newRoomMessage(irc, "<" + contact[0] + "> " + message).send()
+			typ, response = conn.store(num, '+FLAGS', r'(\Seen)')
+	finally:
+		try:
+			conn.close()
+		except:
+			pass
+		conn.logout()
+
 
 
 def getContactIndex (contactList, contact):
@@ -63,6 +104,7 @@ def getQuery(contactList, message, messagePart):
 def main(irc):
  	message = irc.lastMessage()
 	contactList = getContactList()
+	readEmail(irc, contactList)
 	if message.botCommand == "contacts":
 		ircMessage.newRoomMessage(irc, "Contacts in system:" + getContacts(contactList).replace("|",",")).send()		
 	if message.body != None:
@@ -73,7 +115,7 @@ def main(irc):
 			if contact:
 				try:
 					print message.sendingNick
-					sendMail(contactList,contact,message.sendingNick + " says " + text)
+					sendMail(contactList,contact,"<" + message.sendingNick + "> " + text)
 					ircMessage.newRoomMessage(irc, "Text message sent to " + contact).send()
 				except:
 					return
